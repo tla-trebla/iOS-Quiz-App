4958a94f25675ca357c8295d836b031124a72b75
Albert Pangestu <stamina.left@gmail.com>
2021-12-20 15:58:49 +0700

    Initial Commit

---
+++ Kuis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kuis
+//
+//  Created by Albert Pangestu on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
