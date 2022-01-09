1432255d3deac5e25ca23f412794e4698484a4d9
Albert Pangestu <stamina.left@gmail.com>
2022-01-09 23:34:15 +0700

    Created new test to load questions interactor.

---
+++ KuisTests/Interactor/LoadQuestionsInteractorTests.swift
@@ -0,0 +1,14 @@
+//
+//  LoadQuestionsInteractorTests.swift
+//  KuisTests
+//
+//  Created by Albert Pangestu on 09/01/22.
+//
+
+import XCTest
+
+class LoadQuestionsInteractorTests: XCTestCase {
+
+    
+
+}
