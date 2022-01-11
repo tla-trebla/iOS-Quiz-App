a94a14e2cc26123db6e3f42ee8a3a24d59b031cb
Albert Pangestu <stamina.left@gmail.com>
2022-01-11 17:36:30 +0700

    Created APIStore conforms Store protocol and have an error enum named StoreError.

--- Kuis/Store/APIStore.swift
+++ Kuis/Store/APIStore.swift
@@ -7,3 +7,35 @@
 
 import Foundation
 
+enum StoreError: Error {
+    case invalidURL
+}
+
+struct APIStore: Store {
+    
+    typealias Entity = Question
+    
+    func fetch() throws -> [Entity] {
+        
+        var questions = Array(repeating: Question(), count: 10)
+        
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10&category=28&type=multiple") else {
+            throw StoreError.invalidURL
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let data = data, let questionsJSON = try? JSONDecoder().decode([CodableQuestion].self, from: data) {
+                
+                for (index, item) in questionsJSON.enumerated() {
+                        
+                    questions[index].updateQuestion(item.question)
+                    questions[index].updateCorrectAnswer(item.correct_answer)
+                    try? questions[index].addIncorrectAnswers(item.incorrect_answers)
+                }
+            }
+        }.resume()
+        
+        return questions
+    }
+}
