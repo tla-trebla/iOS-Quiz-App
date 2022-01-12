6972e9a39f8d16ee909545c1f5b8207c851679f8
Albert Pangestu <stamina.left@gmail.com>
2022-01-12 15:14:40 +0700

    Created constant file so it'll be used to every module.

---
+++ Kuis/Constants/AppConstants.swift
@@ -0,0 +1,11 @@
+//
+//  AppConstants.swift
+//  Kuis
+//
+//  Created by Albert Pangestu on 12/01/22.
+//
+
+import Foundation
+
+// TODO: Add custom API with custom amount and categories.
+let API_OPEN_TRIVIA_DB = "https://opentdb.com/api.php?amount=10&category=28&type=multiple"
